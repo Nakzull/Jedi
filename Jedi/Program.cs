4945ed02266fd3e8c691666945c9483ee284c9f6
Nakzull <andr25f4@zbc.dk>
2021-08-31 13:09:26 +0200

    Add project files.

---
+++ Jedi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Jedi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> myDictionary = new Dictionary<string, int>();
+            myDictionary.Add("Andreas", 35);
+            myDictionary["Mads"] = 25;
+            myDictionary.Add("Hans Solo", 88);
+
+            myDictionary.Remove("Hans Solo");
+            
+
+            foreach (KeyValuePair<string, int> items in myDictionary)
+                Console.WriteLine("{0}, {1}", items.Key, items.Value);
+        }
+    }
+}
